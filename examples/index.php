79802af103a8d34dbda1887585a542ad191445aa
Andrew Throener <athroener@andrew-ubuntu-virtualBox.(none)>
2014-01-21 15:51:25 -0600

    Added express checkout to examples index

--- examples/index.php
+++ examples/index.php
@@ -2,5 +2,8 @@
 <div><a href="directpayments/sale.php">Sale Transaction</a></div>
 <div><a href="directpayments/authorization.php">Authorization Transaction</a></div>
 
+<h3>Express Checkout</h3>
+<div><a href="expresscheckout/setexpresscheckout.php">Set Express Checkout</a></div>
+
 <h3>Recurring Billing Examples</h3>
 <div><a href="recurringbilling/create.php">Create Recurring Billing Profile</a></div>

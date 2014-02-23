a625d52df6e00971a722e55ce4b19fb762311830
Andrew Throener <athroener@andrew-ubuntu-virtualBox.(none)>
2014-02-23 15:44:14 -0600

    Added credentials file

---
+++ config/credentials.php
@@ -0,0 +1,7 @@
+<?php 
+$config['credentials'] = array(
+	'PARTNER'	=>	'',         //Your Partner.  Usually "PayPal"
+	'VENDOR'	=>	'',         //Your Merchant login
+	'USER'	=> 		'',      	//Your user.  If you followed the directions above it will be the username you created
+	'PWD' => 		'',			//Password for the user
+);
